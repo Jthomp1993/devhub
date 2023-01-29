a592b3c2db5210efbdca0750d2b61775ea31ad20
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2023-01-29 14:57:41 +0000

    Add hover animation to repo item component

--- components/RepoItem.jsx
+++ components/RepoItem.jsx
@@ -12,6 +12,17 @@ const StyledRepoItem = styled.div`
         text-decoration: none;
         transition: .3s ease-in-out;
         
+        vertical-align: middle;
+        -webkit-transform: perspective(1px) translateZ(0);
+        transform: perspective(1px) translateZ(0);
+        box-shadow: 0 0 1px rgb(0 0 0 / 0%);
+        position: relative;
+        -webkit-transition-property: color;
+        transition-property: color;
+        -webkit-transition-duration: 0.3s;
+        transition-duration: 0.3s;
+        
+        
 
         h2 {
             font-size: 24px;
@@ -21,8 +32,29 @@ const StyledRepoItem = styled.div`
         }
     }
 
-    a:hover {
-        background-color: var(--red);
+    a::before {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: var(--red);
+        -webkit-transform: scaleY(0);
+        transform: scaleY(0);
+        -webkit-transform-origin: 50% 0;
+        transform-origin: 50% 0;
+        -webkit-transition-property: transform;
+        transition-property: transform;
+        -webkit-transition-duration: 0.5s;
+        transition-duration: 0.5s;
+        -webkit-transition-timing-function: ease-out;
+        transition-timing-function: ease-out;
+    }
+
+    a:hover::before {
+        transform: scaleY(1);
     }
 
     .stats {
