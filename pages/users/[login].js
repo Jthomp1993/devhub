5b873b99fff3d0e90a3013c33f459950e3a42b9f
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2023-01-29 15:00:10 +0000

    Update meta data in profile page

--- pages/users/[login].js
+++ pages/users/[login].js
@@ -20,7 +20,7 @@ const Profile = ({ user, repos }) => {
   return (
     <>
     <Head>
-        <title>DevHub - {user.login}</title>
+        <title>DevHub - Profile</title>
         <meta name="description" content="DevHub is a web app which enables users to source developers across the globe." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
